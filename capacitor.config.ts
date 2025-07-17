2f731f0eb11c0d033ebcdf81803cc40a7c1e92d7
Kingsley Anamelechi <kingsleyanamelechi422@gmail.com>
2025-07-17 22:31:53 +0100

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.paepros.app',
-  appName: 'PaePros',
+  appId: 'com.pae.etnit',
+  appName: 'PAE',
   webDir: 'dist'
 };
 
